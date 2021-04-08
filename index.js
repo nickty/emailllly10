23b4ad8be0f5c378314c6197e777ca229cad5f46
Mizanur Rahman <nickty.86@gmail.com>
2021-04-08 06:25:18 +0600

    change ind

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send({'hi': 'Mizan'})
+    res.send({'hi': 'Mizan, how are you'})
 })
 
 const PORT = process.env.PORT || 5000
