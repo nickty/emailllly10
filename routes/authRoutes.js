37688391cfba0c1b37c7c90c07138b7bb0480a04
Mizanur Rahman <nickty.86@gmail.com>
2021-04-11 10:23:05 +0600

    logout working

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,6 +11,11 @@ module.exports = (app) => {
     app.get('/api/curuser', (req, res) => {
         res.send(req.user)
     })
+
+    app.get('/api/logout', (req, res) => {
+        req.logout()
+        res.send(req.user)
+    })
 }
 
 
