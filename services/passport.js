b662b33d6ab2ac10bcef8cee6fb93bb11f0d8553
Mizanur Rahman <nickty.86@gmail.com>
2021-04-12 17:27:25 +0600

    front end and concurrently added

--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,8 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: 'https://emailly1000.herokuapp.com/auth/google/callback',
+    callbackURL: '/auth/google/callback',
+    proxy: true
     
 }, (accessToken, refreshToken, profile, done) => {
 
